9ee91a52eaacea63566882811fa108b7007102a5
nsg716 <1029cjswo@naver.com>
2026-01-19 11:51:47 +0900

    9. signup Controller

---
+++ src/main/java/com/aivle/ai0917/ipai/domain/user/controller/SignupController.java
@@ -0,0 +1,7 @@
+package com.aivle.ai0917.ipai.domain.user.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/v1/signup")
+public class SignupController {
+}
